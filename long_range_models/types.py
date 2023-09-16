fa247672b557c1af179388632de1f098949d16cb
Gabriel Faria <gabfaria.cs@gmail.com>
2023-09-16 08:36:30 -0300

    add initializer type

--- long_range_models/types.py
+++ long_range_models/types.py
@@ -1,9 +1,14 @@
 """This module defines generic types used as annotations throughout the library."""
 
-from typing import Callable
+from typing import Any, Callable
 
 import jax
 
 Array = jax.Array
+PRNGKey = Any
+Shape = Any
+DType = Any
+
+Initializer = Callable[[PRNGKey, Shape, DType], Array]
 
 SequenceLayer = Callable[[int], Callable[[Array], Array]]
